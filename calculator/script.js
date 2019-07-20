7471808c2ee4118f41bfd4dfd7b4d1ac05ba190a
Zachary Kravits <49537829+zakuxdesign@users.noreply.github.com>
2019-07-20 19:16:59 -0400

    Happy Path --> Calculator

---
+++ calculator/script.js
@@ -0,0 +1,80 @@
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+const display = document.querySelector(".calculator__display");
+
+keys.addEventListener("click", e => {
+  if (e.target.matches("button")) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
+
+    // Remove .is-depressed class from all keys
+    Array.from(key.parentNode.children).forEach(k =>
+      k.classList.remove("is-depressed")
+    );
+
+    if (!action) {
+      if (displayedNum === "0") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+
+    if (!action) {
+      if (displayedNum === "0" || previousKeyType === "operator") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      key.classList.add("is-depressed");
+      //Clearing first operator is-depressed
+      calculator.dataset.previousKeyType = "operator";
+      //Storing first number
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    }
+
+    if (action === "decimal") {
+      display.textContent = displayedNum + keyContent;
+    }
+
+    if (action === "calculate") {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      const calculate = (n1, operator, n2) => {
+        let result = "";
+
+        if (operator === "add") {
+          result = parseFloat(n1) + parseFloat(n2);
+        } else if (operator === "subtract") {
+          result = parseFloat(n1) - parseFloat(n2);
+        } else if (operator === "multiply") {
+          result = parseFloat(n1) * parseFloat(n2);
+        } else if (operator === "divide") {
+          result = parseFloat(n1) / parseFloat(n2);
+        }
+
+        return result;
+      };
+
+      display.textContent = calculate(firstValue, operator, secondValue);
+    }
+
+    if (action === "clear") {
+      console.log("AC key!");
+    }
+  }
+});
