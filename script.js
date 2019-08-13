4fd92a9f304cb8f6bbb8893c1ca3d20229cf5245
Zachary Allen Kravits <zakcreate@gmail.com>
2019-08-12 20:39:05 -0400

    new edits
    new js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Edge Cases
+
+// Done // If you click "=" with nothing then it should still be 0
+// Done // Hook up the AC key
+
+// // Show an operator key getting clicked repeatedly with CSS changes
+// // If user clicks operator key after second value, the result should show
+// // If user clicks operator key first, then the firstvalue should be negative
+// // If user has input, AC button becomes clear for that step
+
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator__keys");
 const display = document.querySelector(".calculator__display");
@@ -9,6 +19,11 @@ keys.addEventListener("click", e => {
     const keyContent = key.textContent;
     const displayedNum = display.textContent;
     const previousKeyType = calculator.dataset.previousKeyType;
+
+    // Variables used in calculation
+    const firstValue = calculator.dataset.firstValue;
+    const operator = calculator.dataset.operator;
+    const secondValue = displayedNum;
 
     // Remove .is-depressed class from all keys
     Array.from(key.parentNode.children).forEach(k =>
@@ -45,14 +60,7 @@ keys.addEventListener("click", e => {
       calculator.dataset.firstValue = displayedNum;
       calculator.dataset.operator = action;
 
-      if (firstValue && operator && previousKeyType == "operator") {
-        display.textContent = calculate(firstValue, operator, secondValue);
-      }
-
-      key.classList.add("is-depressed");
-      calculator.dataset.previousKeyType = "operator";
-      calculator.dataset.firstValue = displayedNum;
-      calculator.dataset.operator = action;
+      display.textContent = calculate(firstValue, operator, secondValue);
     }
 
     if (action === "decimal") {
@@ -69,10 +77,6 @@ keys.addEventListener("click", e => {
     }
 
     if (action === "calculate") {
-      const firstValue = calculator.dataset.firstValue;
-      const operator = calculator.dataset.operator;
-      const secondValue = displayedNum;
-
       const calculate = (n1, operator, n2) => {
         let result = "";
 
@@ -84,9 +88,21 @@ keys.addEventListener("click", e => {
           result = parseFloat(n1) * parseFloat(n2);
         } else if (operator === "divide") {
           result = parseFloat(n1) / parseFloat(n2);
+        } else if (operator == null) {
+          result = display.textContent;
         }
 
         calculator.dataset.previousKeyType = "calculate";
+
+        if (
+          typeof firstValue !== undefined &&
+          typeof operator !== undefined &&
+          display.textContent !== "0" &&
+          previousKeyType === "operator"
+        ) {
+          // calculate(firstValue, operator, secondValue);
+          key.classList.add("is-depressed");
+        }
 
         return result;
       };
@@ -96,7 +112,6 @@ keys.addEventListener("click", e => {
 
     if (action === "clear") {
       calculator.dataset.previousKeyType = "clear";
-
       calculator.dataset.firstValue = "0";
       calculator.dataset.operator = undefined;
       display.textContent = "0";
